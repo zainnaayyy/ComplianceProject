e87f8e930f3a1bc8def62684b858119bd858db89
zainnaayyy <muhammadzain140013@gmail.com>
2024-07-09 01:12:38 +0500

    Logout + Search APIs for site & LOB

--- compliance-server/Routes/LOBRoute.js
+++ compliance-server/Routes/LOBRoute.js
@@ -1,10 +1,11 @@
-const { addLOB, deleteLOB, getLOBById, getLOBs } = require('../Controllers/LOBController');
+const { addLOB, deleteLOB, getLOBById, getLOBs, searchLOBs } = require('../Controllers/LOBController');
 const { authMiddleware } = require('../Middlewares/AuthMiddleware');
 const router = require("express").Router();
 
-router.post('/addLOB',  addLOB);
-router.get('/getLOBs',  getLOBs);
-router.get('/getLOBById',  getLOBById);
-router.delete('/deleteLOB',  deleteLOB);
+router.post('/addLOB', authMiddleware, addLOB);
+router.get('/getLOBs', authMiddleware, getLOBs);
+router.get('/getLOBById', authMiddleware, getLOBById);
+router.delete('/deleteLOB', authMiddleware, deleteLOB);
+router.get('/searchLOB', authMiddleware, searchLOBs);
 
 module.exports = router;
