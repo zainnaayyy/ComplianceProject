591162a3dd2b491377df51ba71a550f1c194476a
TalhaKazmi1 <talhakazmi694@gmail.com>
2024-07-22 13:06:56 +0500

    modal working

--- compliance-platform/components/ModalComponent.jsx
+++ compliance-platform/components/ModalComponent.jsx
@@ -1,75 +1,72 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button, Modal } from 'antd';
+
 const ModalComponent = ({
   isModalOpen,
   handleOk,
   handleCancel,
   selectedUser,
 }) => {
+  console.log(selectedUser._id, 'selectedUser');
   return (
-    <>
-      <Modal
-        title='User Profile'
-        open={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <div className='flex flex-col items-center'>
-          <img
-            className='rounded-full w-48 h-48 '
-            src='../icons/userImage.png'
-            alt='User'
-          />
-          {/* {selectedUser && (
-            <div className='w-full mt-4 mb-4 space-y-3'>
-              <div className='flex justify-between px-5 text-lg'>
-                <span className='font-bold text-gray-800'>Full Name:</span>
-                <span className='text-gray-800'>{selectedUser.fullName}</span>
-              </div>
-              <div className='flex justify-between px-5 text-lg'>
-                <span className='font-bold text-gray-800'>Email:</span>
-                <span className='text-gray-600'>{selectedUser.email}</span>
-              </div>
-              <div className='flex justify-between px-5 text-lg'>
-                <span className='font-bold text-gray-800'>LOB:</span>
-                <span className='text-gray-600'>{selectedUser.LOB}</span>
-              </div>
-              <div className='flex justify-between px-5 text-lg'>
-                <span className='font-bold text-gray-800'>Site:</span>
-                <span className='text-gray-600'>{selectedUser.site}</span>
-              </div>
-              <div className='flex justify-between px-5 text-lg'>
-                <span className='font-bold text-gray-800'>Status:</span>
-                <span
-                  className={`font-medium ${
-                    selectedUser.status ? 'text-green-600' : 'text-red-600'
-                  }`}
-                >
-                  {selectedUser.status ? 'Active' : 'Inactive'}
-                </span>
-              </div>
-            </div>
-          )} */}
-          {selectedUser && (
-            <div className='text-center space-y-3'>
-              <p className='text-2xl font-bold text-gray-800'>
+    <Modal
+      title='User Profile'
+      open={isModalOpen}
+      onOk={handleOk}
+      onCancel={handleCancel}
+      width={600} // Increased modal width
+    >
+      <div className='flex flex-col items-center'>
+        <img
+          className='rounded-full w-48 h-48 mb-4'
+          src='../icons/userImage.png'
+          alt='User'
+        />
+        {selectedUser && (
+          <div className='grid grid-cols-2 gap-14 content-center'>
+            <div className='flex flex-col '>
+              <span className=' text-lg font-bold text-gray-700'>
+                Full Name
+              </span>
+              <span className='text-lg text-gray-900'>
                 {selectedUser.fullName}
-              </p>
-              <p className='text-lg text-gray-600'>{selectedUser.email}</p>
-              <p className='text-lg text-gray-600'>{selectedUser.LOB}</p>
-              <p className='text-lg text-gray-600'>{selectedUser.site}</p>
-              <p
+              </span>
+            </div>
+            <div className='flex flex-col '>
+              <span className='text-lg font-bold text-gray-700'>Email</span>
+              <span className='text-lg text-gray-900'>
+                {selectedUser.email}
+              </span>
+            </div>
+            <div className='flex flex-col '>
+              <span className='text-lg font-bold text-gray-700'>
+                Line Of Business
+              </span>
+              <span className='text-lg text-gray-900'>{selectedUser.LOB}</span>
+            </div>
+            <div className='flex flex-col '>
+              <span className='text-lg font-bold text-gray-700'>Site</span>
+              <span className='text-lg text-gray-900'>{selectedUser.site}</span>
+            </div>
+            <div className='flex flex-col '>
+              <span className='text-lg font-bold text-gray-700'>Status</span>
+              <span
                 className={`text-lg font-medium ${
                   selectedUser.status ? 'text-green-600' : 'text-red-600'
                 }`}
               >
                 {selectedUser.status ? 'Active' : 'Inactive'}
-              </p>
+              </span>
             </div>
-          )}
-        </div>
-      </Modal>
-    </>
+            <div className='flex flex-col '>
+              <span className='text-lg font-bold text-gray-700'>Role</span>
+              <span className='text-lg text-gray-900'>Developer</span>
+            </div>
+          </div>
+        )}
+      </div>
+    </Modal>
   );
 };
+
 export default ModalComponent;
